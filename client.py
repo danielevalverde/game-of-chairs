604a0a808ce67b130a70a08a4ac3ab6a5cf6481a
joanderson.goncalves <joanderson.goncalves@ufba.br>
2023-07-09 19:26:19 -0300

    partial: add music and some logic

--- client.py
+++ client.py
@@ -1,27 +1,44 @@
+from pydub import AudioSegment
+from pydub.playback import play
 import socket
+from playsound import playsound
+
+import constants
 
 HOST = '127.0.0.1'
 PORT = 5555
 
+def play_music():
+    playsound('music/ciranda-cirandinha.mp3')
+
+
 def play_game():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
-        client_socket.connect((HOST, PORT))
-        print('Conectado ao servidor')
+        global PORT
+        while True:
+            try:
+                client_socket.connect((HOST, PORT))
+                break
+            except:
+                PORT += 1
+
+        print('Executando na porta ' + str(PORT) + '. Conectado ao servidor.')
+
+        # play_music()
 
         while True:
-            guess = input('Adivinhe o número (ou "q" para sair): ')
-
-            if guess == 'q':
-                break
-
-            client_socket.sendall(guess.encode())
+            ready = input('Está pronto para jogar? s/n: ')
+            client_socket.sendall(constants.READY.encode() if ready == 's' else constants.NOT_READY.encode())
             response = client_socket.recv(1024).decode()
 
-            if response.startswith('Correto'):
-                print(response)
+            #Agora eu testo se o comando é para esperar ou começar a música
+            if response == constants.PLAY_MUSIC:
+                print("Tocando música")
+                play_music()
                 break
             else:
                 print(response)
+
 
 if __name__ == '__main__':
     play_game()
---
+++ constants.py
@@ -0,0 +1,4 @@
+PLAY_MUSIC = "play_music"
+STOP_MUSIC = "stop_music"
+READY = "ready"
+NOT_READY = "not_ready"
